fb1d3d07245f67ffe2ea9ab39d56f2d1953ee9b7
SumitGujariya <gujariyasumit96@gmail.com>
2025-11-17 23:46:37 +0530

    push in java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "Sumit";
-        System.out.println("Hello " + name);
+        int a = 10;
+        System.out.println(a);
     }
 }
